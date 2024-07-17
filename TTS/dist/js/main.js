345616c7532ff10c02fd54c45eb8d9057e27a64e
Vinícius Freitas <vinifreitass20@gmail.com>
2024-07-17 19:14:23 -0300

    últimas atualizações de configuração relacionadas a voz

--- TTS/dist/js/main.js
+++ TTS/dist/js/main.js
@@ -10,9 +10,68 @@ const voiceSelect = document.querySelector('#voice-select')
 // init voices array
 let voices = [];
 
-const getVoices = () =>{
+const getVoices = () => {
     voices = synth.getVoices()
     console.log(voices)
+
+    // loop through voices and create an option for each one
+
+    voices.forEach(voice => {
+        // create option element
+        const option = document.createElement('option')
+        // fill option with voice end language
+        option.textContent = voice.name + '(' + voice.lang + ')'
+        // set need option attributes
+        option.setAttribute('data-lang', voice.lang)
+        option.setAttribute('data-name', voice.name)
+        voiceSelect.appendChild(option)
+    })
+}
+getVoices()
+if (synth.onvoiceschanged !== undefined) {
+    synth.onvoiceschanged = getVoices
 }
 
-getVoices
+// speak
+
+const speak = () => {
+    // check if speaking
+    if (synth.speaking) {
+        console.error('Already speaking...')
+        return
+    }
+    if (textInput.value !== '') {
+        // get speak text
+        const speakText = new SpeechSynthesisUtterance(textInput.value)
+
+        // speak end
+        speakText.onend = e => {
+            console.log('Done speaking...')
+        }
+        // speak error
+        speakText.onerror = e => {
+            console.error('Something went wrong')
+        }
+        // selected voice
+        const selectedVoice = voiceSelect.selectedOptions[0].getAttribute('data-name')
+
+        // loop through voices
+        voices.forEach(voice => {
+            if (voice.name === selectedVoice) {
+                speakText.voice = voice
+            }
+        })
+    }
+}
+// event listeners
+
+//text form submit 
+
+textForm.addEventListener('submit', e => {
+    e.preventDefault()
+    speak()
+    textInput.blur()
+})
+
+voiceSelect.addEventListener('change', e => speak())
+
